94cc21a3ec2c98533cd9b8452b2eb7a01ddc4ef9
Farouk <farouk.mokhtar@gmail.com>
2025-03-10 19:57:04 +0100

    up

--- boostedhiggs/wjetsnloprocessor.py
+++ boostedhiggs/wjetsnloprocessor.py
@@ -406,8 +406,8 @@ class VjetsProcessor(processor.ProcessorABC):
 
         # nB0 = (ak.sum(goodgenjets.hadronFlavour == 5, axis=1) == 0).to_numpy()
         # nB1 = (ak.sum(goodgenjets.hadronFlavour == 5, axis=1) == 1).to_numpy()
-        # nB2 = (ak.sum(goodgenjets.hadronFlavour == 5, axis=1) == 2).to_numpy()
-        nB2 = (ak.num(goodgenjets.hadronFlavour == 5, axis=1)).to_numpy()
+        nB2 = (ak.sum(goodgenjets.hadronFlavour == 5, axis=1) == 2).to_numpy()
+        nB2_new = (ak.num(goodgenjets.hadronFlavour == 5, axis=1)).to_numpy()
 
         nC0 = (ak.sum(goodgenjets.hadronFlavour == 4, axis=1) == 0).to_numpy()
         nC1 = (ak.sum(goodgenjets.hadronFlavour == 4, axis=1) == 1).to_numpy()
@@ -420,6 +420,7 @@ class VjetsProcessor(processor.ProcessorABC):
         variables = {
             # "nB0": nB0,
             # "nB1": nB1,
+            "nB2_new": nB2_new,
             "nB2": nB2,
             "nC0": nC0,
             "nC1": nC1,
